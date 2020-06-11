ad467c50e5762004c224983fb4f29dc5b5191d09
GaylordOudry <gaylord.oudry@gmail.com>
2020-06-11 22:35:25 +0200

    This is my first website using Vue.js

--- exercise-small-website/src/main.js
+++ exercise-small-website/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routing from './Routing'
+
+// Importation de boostrap
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Initialisation du router
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: Routing,
+  mode: 'history'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
